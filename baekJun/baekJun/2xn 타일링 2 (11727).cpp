492c1c850a629ddb34bab9fb54e62148b52241e6
DuHyeonChoi <engus9902@gmail.com>
2023-02-03 01:52:52 +0900

    2*n 타일링 solved

--- baekJun/baekJun/2xn 타일링 2 (11727).cpp
+++ baekJun/baekJun/2xn 타일링 2 (11727).cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-
 using namespace std;
 
+int dp[1001] = { 0, 1, 3, 0, };
+
 int main(void) {
-	int dp[1001] = { 1, 3, 0, };
 	int n;
 	cin >> n;
 
-
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+	}
+	cout << dp[n];
 }
