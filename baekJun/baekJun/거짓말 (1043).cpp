3fb1ad6c3fbc79d277935d6d6486bbaa26cb05af
DuHyeonChoi <engus9902@gmail.com>
2023-02-14 16:02:30 +0900

    거짓말 solved 2023.02.14

--- baekJun/baekJun/거짓말 (1043).cpp
+++ baekJun/baekJun/거짓말 (1043).cpp
@@ -1,41 +1,76 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-bool find(int arr[], int len, int key) {
-	for (int i = 0; i < len; i++) {
-		if (arr[i] == key) {
-			return true;
-		}
+/* 사람들[사람번호][진실 인지 여부]
+	ex) people[15][0] : 15번째 사람이랑 연결된 노드 값
+		people[26][1] : 26번째 사람이 진실 인지 여부, 모른다면 0
+*/
+int people[52][2];
+int people_in_party[52][52];
+int Find(int key) {
+	if (people[key][0] == key) {
+		return key;
 	}
-	return false;
+	return Find(people[key][0]);
+}
+
+void Union(int x, int y) {
+	x = Find(x); // prev
+	y = Find(y); // next
+	people[x][0] = y;
+	if (people[x][1] == 1) {
+		people[y][1] = 1;
+	}
+	if (people[y][1] == 1) {
+		people[x][1] = 1;
+	}
 }
 
 int main(void) {
-	int N, M; // N : 사람수, M : 파티수
-	int truth_num; // 진실을 아는 사람 수
-	int a, b, c;
-	int know_person[51] = { 0, }; // 진실을 아는 사람 목록
-	int party_people[51][51] = {0,}; //파티 명단
-	int party_exagg[51] = { 0, }; //과장가능 파티
-
-	cin >> N >> M;
-	cin >> truth_num;
-	for (int i = 0; i < truth_num; i++) {
-		cin >> a;
-		know_person[i] = a;
+	int N, M;
+	int a; /*진실을 아는 사람 수*/
+	int b; /*진실을 아는 사람 번호*/
+	int c; /*각 파티원 수*/
+	int d = 0; /*각 파티원 번호*/
+	int prev, flag = 0, ans = 0;
+	cin >> N >> M; // 입력 1-1, 1-2
+	ans = M;
+	for (int i = 1; i <= N; i++) {
+		people[i][0] = i;
 	}
-
-	for (int i = 0; i < M; i++) {
-		cin >> b; //파티 참가원 수
-		for (int j = 0; j < b; j++) {
-			cin >> c; //참가자 번호
-			party_people[i][j] = c;
-			if (!find(know_person, truth_num, c)) { //진실을 아는 사람이 없다면
-				party_exagg[j] = 1; //j번째 파티는 거짓말 가능
+	cin >> a; // 입력 2-1
+	for (int i = 0; i < a; i++) { // 입력 2-2~
+		cin >> b;
+		people[b][1] = 1;
+	}
+	for (int i = 0; i < M; i++) { // 입력 3~
+		cin >> c;
+		people_in_party[i][0] = c;
+		for (int j = 1; j <= c; j++) {
+			if (j > 1) {
+				prev = d;
+				cin >> d;
+				Union(prev, d);
 			}
+			else {
+				cin >> d;
+			}
+			people_in_party[i][j] = d;
 		}
 
-
 	}
+	for (int i = 0; i < M; i++) {
+		flag = 0;
+		for (int j = 1; j <= people_in_party[i][0]; j++) {
+			if (people[Find(people_in_party[i][j])][1] == 1 || people[people_in_party[i][j]][1] == 1) {
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 1) {
+			ans--;
+			continue;
+		}
+	}
+	cout << ans;
 }
--- baekJun/baekJun/백준 연습용.cpp
+++ baekJun/baekJun/백준 연습용.cpp
@@ -1,17 +1,94 @@
 //#include <iostream>
 //using namespace std;
 //
-//bool find(int arr[], int len, int key) {
-//	for (int i = 0; i < len; i++) {
-//		if (arr[i] == key) {
-//			return true;
-//		}
+///* 사람들[사람번호][진실 인지 여부]
+//	ex) people[15][0] : 15번째 사람이랑 연결된 노드 값
+//		people[26][1] : 26번째 사람이 진실 인지 여부, 모른다면 0
+//*/
+//int people[52][2];
+//int people_in_party[52][52];
+//int Find(int key) {
+//	if (people[key][0] == key) {
+//		return key;
 //	}
-//	return false;
+//	return Find(people[key][0]);
+//}
+//
+//void Union(int x, int y) {
+//	x = Find(x); // prev
+//	y = Find(y); // next
+//	people[x][0] = y;
+//	if (people[x][1] == 1) {
+//		people[y][1] = 1;
+//	}
+//	if (people[y][1] == 1) {
+//		people[x][1] = 1;
+//	}
 //}
 //
 //int main(void) {
-//	int hello[10] = { 0, 0, 3, 4, 5, 6 };
-//	cout << sizeof(hello) / sizeof(int) << "\n";
-//	cout << find(hello, 6, 3);
+//	int N, M;
+//	int a; /*진실을 아는 사람 수*/
+//	int b; /*진실을 아는 사람 번호*/
+//	int c; /*각 파티원 수*/
+//	int d = 0; /*각 파티원 번호*/
+//	int prev, flag = 0, ans = 0;
+//	cin >> N >> M; // 입력 1-1, 1-2
+//	ans = M;
+//	for (int i = 1; i <= N; i++) {
+//		people[i][0] = i;
+//	}
+//	cin >> a; // 입력 2-1
+//	for (int i = 0; i < a; i++) { // 입력 2-2~
+//		cin >> b;
+//		people[b][1] = 1;
+//	}
+//	for (int i = 0; i < M; i++) { // 입력 3~
+//		cin >> c;
+//		people_in_party[i][0] = c;
+//		for (int j = 1; j <= c; j++) {
+//			if (j > 1) {
+//				prev = d;
+//				cin >> d;
+//				Union(prev, d);
+//			}
+//			else {
+//				cin >> d;
+//			}
+//			people_in_party[i][j] = d;
+//		}
+//
+//	}
+//	cout << "peoples : ";
+//	for (int i = 1; i <= N; i++) {
+//		cout << people[i][0] << " ";
+//	}
+//	cout << "\n";
+//	for (int i = 0; i < M; i++) {
+//		flag = 0;
+//		for (int j = 1; j <= people_in_party[i][0]; j++) {
+//			cout << i + 1 << "번째 파티의 " << j << "번째 사람 : " << people_in_party[i][j] << " ";
+//			cout << " -> 헤드 : " << Find(people_in_party[i][j]);
+//			if (people[Find(people_in_party[i][j])][1] == 1 || people[people_in_party[i][j]][1] == 1) {
+//				flag = 1;
+//				break;
+//			}
+//		}
+//		if (flag == 1) {
+//			ans--;
+//			cout << " /// ans : " << ans << "\n";
+//			continue;
+//		}
+//		cout << "\n";
+//	}
+//	cout << ans;
+//	/*for (int i = 0; i < M; i++) {
+//		for (int j = 0; j < 5; j++) {
+//			cout << people_in_party[i][j] << " ";
+//		}
+//		cout << "\n";
+//	}*/
+//	/*for (int i = 1; i <= N; i++) {
+//		cout << people[i][0] << " ";
+//	}*/
 //}
